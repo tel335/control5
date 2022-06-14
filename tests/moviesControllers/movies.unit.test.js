a5b41bdf31e9a3de8997f6e390e9f445b6d425a2
BENJAMIN <benjamin.pinoo@sansano.usm.cl>
2022-06-13 21:09:47 -0400

    FINAL final commit (definitive)

--- tests/moviesControllers/movies.unit.test.js
+++ tests/moviesControllers/movies.unit.test.js
@@ -12,26 +12,9 @@ describe("Tests unitarios", () => {
         expect(response).toEqual([]);
     });
 
-    test("Deberia retornar los datos del padrino", () => {
-        const response = movies.getMoviesByName("The Godfather");
-        expect(response[0]).toEqual({
-            "Title": "The Godfather",
-            "US Gross": 134966411,
-            "Worldwide Gross": 268500000,
-            "US DVD Sales": null,
-            "Production Budget": 7000000,
-            "Release Date": "Mar 15 1972",
-            "MPAA Rating": null,
-            "Running Time min": null,
-            "Distributor": "Paramount Pictures",
-            "Source": null,
-            "Major Genre": null,
-            "Creative Type": "Historical Fiction",
-            "Director": "Francis Ford Coppola",
-            "Rotten Tomatoes Rating": 100,
-            "IMDB Rating": 9.2,
-            "IMDB Votes": 411088
-        });
+    test("Deberia retornar los datos de Little Women", () => {
+        const response = movies.getMoviesByName("Little Women");
+        expect(response[0]).toEqual({"Title": "Little Women", "US Gross": 50003303, "Worldwide Gross": 50003303, "US DVD Sales": null, "Production Budget": 15000000, "Release Date": "Dec 21 1994", "MPAA Rating": "PG", "Running Time min": null, "Distributor": "Sony Pictures", "Source": "Based on Book/Short Story", "Major Genre": "Drama", "Creative Type": "Historical Fiction", "Director": null, "Rotten Tomatoes Rating": 89, "IMDB Rating": 7.1, "IMDB Votes": 16514});
     })
 
     test("Deberia retornar las peliculas ordenadas en orden decendiente segun imdb", () => {
@@ -55,5 +38,10 @@ describe("Tests unitarios", () => {
             "IMDB Rating": 9.2,
             "IMDB Votes": 411088
         });
+    });
+
+    test("Deberia retornar las peliculas en orden ascendiente segun rotten tomatoes", () => {
+        const response = movies.getMoviesByClassifier('rotten', 'asc');
+        expect(response).toEqual(list);
     });
 });
