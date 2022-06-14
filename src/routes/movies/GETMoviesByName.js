7debeb2e8e14f2484ed9ce2ee4edc6bfba1ebc72
Victor Capetillo <victor.capetillo@usm.cl>
2022-06-13 21:32:28 -0400

    Control Listo

--- src/routes/movies/GETMoviesByName.js
+++ src/routes/movies/GETMoviesByName.js
@@ -3,7 +3,15 @@ import { getOKResponse, getErrorResponse } from '../../utils/responseBuilder'
 
 exports.GETMoviesByName = (ctx) => {
     const moviesList = moviesController.getMoviesByName(ctx.params.name)
-    if (moviesList) ctx = getOKResponse(ctx, moviesList) 
-    else ctx = getErrorResponse(ctx)
+    if (moviesList.length == 0){
+        ctx.body = { status: 200, message: 'No se han encontrado coincidencias' }
+        ctx.status = 200
+    }
+    else if (moviesList){
+        ctx = getOKResponse(ctx, moviesList)
+    }
+    else{
+        ctx = getErrorResponse(ctx)
+    }
     return ctx
 }
